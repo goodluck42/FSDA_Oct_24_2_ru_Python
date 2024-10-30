7761355baa0d410823f545a3f971ef2652d823fc
Alex <xrlogic1@gmail.com>
2024-10-30 17:10:16 +0400

    Initial commit

---
+++ DataTypes/main.py
@@ -0,0 +1,39 @@
+# +, -, *, /, **, //, %
+
+age = 25 # int
+name = "Ali" # str 65, 108, 105
+gender = 'M' # char
+money = 10.5 # float
+is_educated = True # bool
+
+###################### age
+# print((age + 2) * 2)
+# print(age)
+
+# age = age * 2
+# age *= 2 # shorthand operators
+# age += 4
+# age -= 10
+# age **= 2 # 8836
+# age %= 10
+#
+# print(age)
+
+###################### name
+
+name = name + "baba" # concatenation / конкатенация
+
+print(name)
+
+text = "br" * 2
+
+print(text * 2)
+
+
+### Escape sequences
+print("\\t - \t tabulation")
+print("\\n - \n newline")
+print("\\ - escaping")
+
+
+
---
+++ Intro/main.py
@@ -0,0 +1,22 @@
+# Ввод с клавиатуры
+# count_of_apples = input() # snake case
+# countOfApples = input() # camel case
+# count-of-apples = input() # kebab case
+# CountOfApples = input() # pascal case
+
+# Вывод переменной age
+# print(count_of_apples)
+
+
+############################
+# concatenation
+number1 = int(input("Enter number1: ")) # str -> int
+number2 = int(input("Enter number2: ")) # str -> int
+
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(number1 // number2)
+# print(number1 ** number2)
+# print(number1 % number2)
