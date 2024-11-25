87691aa5ceb463265c65547b025ad33566196dcb
Alex <xrlogic1@gmail.com>
2024-11-25 16:58:11 +0400

    Endless Loop

---
+++ EndlessLoop/main.py
@@ -0,0 +1,46 @@
+# a = 0
+# saved_a = a
+# while a < 5:
+#     a += 1
+#     print(a)
+# # <------ a = 5
+# a = saved_a
+#
+# while a < 5:
+#     a += 1
+#     print(a)
+########################
+# i = 0
+# while True:
+#     i += 1
+#
+#     if i >= 800_000:
+#         break
+#
+#     print(i)
+#
+#     i += 1
+#
+# print("Done")
+
+# i = 0
+#
+# while i <= 5:
+#     if i == 3 or i == 1:
+#         i += 1
+#         continue
+#
+#     print(i)
+#     i += 1
+#
+# print("Don")
+
+from random import randint
+
+# random number
+a = randint(1, 100)
+
+print(a)
+
+
+
